8994d8fa7d31a1caaf0b2142a1f0b8baf9f75f44
Cissica Gonzalez Martinez <cecemartinez@hotmail.com>
2020-08-06 16:14:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    katz_deli.each_with_index do | customer , index |
+      string << " #{index+1}. #{customer}"
+    end
+    puts string
+  end
+end
+def take_a_number(katz_deli,name)
+  puts "Welcome, #{name}. You are number #{katz_deli+1} in line."
+end
