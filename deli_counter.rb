480e3b7b84f8a176aca7eac3e2005b7169b65277
Cissica Gonzalez Martinez <cecemartinez@hotmail.com>
2020-08-06 16:59:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,10 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+def now_serving
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  end
 
 end
